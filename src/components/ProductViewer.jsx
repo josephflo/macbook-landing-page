ece32eae30fc2364ce4215c38de7dad337b47c1a
josephflo <josephflo.dev@gmail.com>
2025-10-15 08:14:06 -0300

    add/threejs model box

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -1,30 +1,74 @@
+import clsx from "clsx";
+import useDevBookStore from "../store";
+import { Canvas } from "@react-three/fiber";
+import { Box, OrbitControls  } from "@react-three/drei";
+
 const ProductViewer = () => {
+  const { color, scale, setColor, setScale } = useDevBookStore();
+
   return (
     <section id="product-viewer">
       <h2>Take a closer look.</h2>
 
       <div className="controls">
-        <p className="info">DevBookPro 16" Black / Silver</p>
+        <p className="info">
+          DevBookPro {scale}" in {color}
+        </p>
 
         <div className="flex-center gap-5 mt-5">
           <div className="color-control">
-            <div className="bg-neutral-300" />
-            <div className="bg-neutral-900" />
+            <div
+              onClick={() => setColor("#adb5bd")}
+              className={clsx(
+                "bg-neutral-300",
+                color === "#adb5bd" && "active"
+              )}
+            />
+            <div
+              onClick={() => setColor("#2e2c2e")}
+              className={clsx(
+                "bg-neutral-900",
+                color === "#2e2c2e" && "active"
+              )}
+            />
           </div>
 
           <div className="size-control">
-            <div>
+            <div
+              onClick={() => setScale(0.06)}
+              className={clsx(
+                scale === 0.06
+                  ? "bg-white text-black"
+                  : "bg-transparent text-white"
+              )}
+            >
               <p>14"</p>
             </div>
-            <div>
+            <div
+              onClick={() => setScale(0.08)}
+              className={clsx(
+                scale === 0.08
+                  ? "bg-white text-black"
+                  : "bg-transparent text-white"
+              )}
+            >
               <p>16"</p>
             </div>
           </div>
-
         </div>
       </div>
 
-      <p className="text-white text-4xl">Render Canvas</p>
+      <Canvas
+        id="canvas"
+        camera={{ position: [0, 0, 10], fov: 50, near: 0.1, far: 100 }}
+      >
+        <Box
+          position={[0, 0, 0]}
+          scale={10 * scale}
+          material-color={color}
+        ></Box>
+        <OrbitControls enableZoom={false} />
+      </Canvas>
     </section>
   );
 };
